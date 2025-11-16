852880bb6c1b3f696a91e70fa0d5709f843d2329
Yoan <yoandiny14@gmail.com>
2025-11-16 13:18:18 +0300

    initialized Student and Tutor

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,12 @@
+import java.util.Date;
+
 public class Student {
+    Integer id;
+    String lastName;
+    String firstName;
+    Date birthDate;
+    String mail;
+    String phone;
+    String group;
+    Tutor tutor;
 }
--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -1,2 +1,11 @@
+import java.util.Date;
+
 public class Tutor {
+    Integer id;
+    String lastName;
+    String firstName;
+    Date birthDate;
+    String mail;
+    String phone;
+    String link;
 }
