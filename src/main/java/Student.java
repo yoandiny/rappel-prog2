c06f89d610aac8003b972f8cbdbf7ea735caba02
Yoan <yoandiny14@gmail.com>
2025-11-17 20:27:24 +0300

    added Student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,12 +1,15 @@
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
 
-public class Student {
-    Integer id;
-    String lastName;
-    String firstName;
-    Date birthDate;
-    String mail;
-    String phone;
-    String group;
-    Tutor tutor;
+public class Student extends Person {
+    private String group;
+    private Tutor tutor;
+
+    public Student(int id, String lastName, String firstName, LocalDate birthDate, String email, String phoneNumber, String group, Tutor tutor) {
+        super(id, lastName, firstName, birthDate, email, phoneNumber);
+        this.group = group;
+        this.tutor = tutor;
+    }
+
+
 }
