124ffddeada66ff228112431e7e35f8d19d8c8e9
Yoan <yoandiny14@gmail.com>
2025-11-17 20:24:22 +0300

    added Tutor full logic

--- src/main/java/Tutor.java
+++ src/main/java/Tutor.java
@@ -1,11 +1,12 @@
-import java.util.Date;
+import java.time.LocalDate;
 
-public class Tutor {
-    Integer id;
-    String lastName;
-    String firstName;
-    Date birthDate;
-    String mail;
-    String phone;
-    String link;
+public class Tutor extends Person {
+    private String relationship;
+
+    public Tutor(int id, String lastName, String firstName, LocalDate birthDate, String email, String phoneNumber, String relationship) {
+        super(id, lastName, firstName, birthDate, email, phoneNumber);
+        this.relationship = relationship;
+    }
+
+    public String getRelationship() { return relationship; }
 }
