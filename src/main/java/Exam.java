039824166db29f994bd3e8b835c58fe55df8e1db
Yoan <yoandiny14@gmail.com>
2025-11-17 22:11:45 +0300

    added Exam class

--- src/main/java/Exam.java
+++ src/main/java/Exam.java
@@ -1,2 +1,19 @@
+import java.time.Instant;
+import java.util.Objects;
+
 public class Exam {
+    private int id;
+    private String title;
+    private Course course;
+    private Instant examDateTime;
+    private double coefficient;
+
+    public Exam(int id, String title, Course course, Instant examDateTime, double coefficient) {
+        this.id = id;
+        this.title = title;
+        this.course = course;
+        this.examDateTime = examDateTime;
+        this.coefficient = coefficient;
+    }
+
 }
